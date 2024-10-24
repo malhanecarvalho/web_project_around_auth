c29673a2ac7d271db0793ad4d62504d0b10241ef
malhane carvalho <malhanec@gmail.com>
2024-10-24 16:01:26 +0100

    feature: altera api para as funções de register e login

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://register.nomoreparties.co";
+const BASE_URL = "http://localhost:3000/auth";
 
 export const register = async ({ email, password }) => {
   return fetch(`${BASE_URL}/signup`, {
